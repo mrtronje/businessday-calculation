a6b11d682c100e8103638baae495354ac6a9360a
mrtronje <github@tronje.ch>
2019-08-08 18:03:02 +0200

    Implement some helpers

--- NumericHelper.php
+++ NumericHelper.php
@@ -1,1 +1,36 @@
+<?php
 
+
+class NumericHelper {
+  
+    private static function nearestPeriodicValue($point, $value, $period)
+    {
+        $relation = ($value - $point) / $period;
+        $equidistant = !(fmod($relation, 0.5)) && $relation % 1;
+        $mod = $equidistant ? $period : 0;
+        return $mod + ($value - $period * round($relation));
+    }
+    
+    public static function containedPeriodicValues(Carbon $start, Carbon $end, $value, $period): int
+    {
+        if ($start === $end) {
+            return 0;
+        }
+        if ($start > $end) {
+            $newEnd = $start;
+            $start = $end;
+            $end = $newEnd;
+        }
+        $end--;
+        $nearest = self::nearestPeriodicValue($start, $value, $period);
+        if ($nearest - $start < 0) {
+            $nearest += $period;
+        }
+        if (($nearest - $start) > ($end - $start)) {
+            return 0;
+        } else {
+            return 1 + (int)(($end - $nearest) / $period);
+        }
+    } 
+  
+}
